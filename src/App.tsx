12c1720d12ec0a55a5c5f76d5bb43433483e3703
Nhan Cao <nhancv92@gmail.com>
2018-03-12 09:50:14 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import * as React from 'react';
-import './App.css';
+import * as React from 'react'
+import './App.css'
 
-const logo = require('./logo.svg');
+// const logo = require('./profile.jpg')
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-logo" />
+          <div className="App-header-info">
+            <h1 className="App-title">Nhan Cao</h1>
+            <h2 className="App-subtitle">Mobile Developer</h2>
+            <h2 className="App-subtitle">nhancv92@gmail.com</h2>
+          </div>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
