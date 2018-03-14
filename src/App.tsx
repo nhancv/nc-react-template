ae854c24781226f6d448d2907895c63c2d991f9b
Nhan Cao <nhancv92@gmail.com>
2018-03-14 09:51:37 +0700

    fix particle, add react bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import * as React from 'react'
 import './App.css'
 import Particles from 'react-particles-js'
+import { Button } from 'reactstrap'
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div className="App-logo" />
-          <div className="App-header-info">
-            <h1 className="App-title">Nhan Cao</h1>
-            <h2 className="App-subtitle">Mobile Developer</h2>
-            <h2 className="App-subtitle">nhancv92@gmail.com</h2>
+          <Particles className="App-particle" />
+
+          <div className="App-header-body">
+            <div className="App-logo" />
+            <div className="App-header-info">
+              <h1 className="App-title">Nhan Cao</h1>
+              <h2 className="App-subtitle">Mobile Developer</h2>
+              <h2 className="App-subtitle">nhancv92@gmail.com</h2>
+            </div>
           </div>
-          <Particles />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className="App-body">
+          <section>
+            <p>
+              To get started, edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <div>
+              <Button
+                color="primary"
+                onClick={() => {
+                  console.log('hello')
+                }}
+              >
+                Hello
+              </Button>
+            </div>
+          </section>
+        </div>
+
+        <footer className="App-footer">
+          <div>
+            <br />
+            <small>---- Nhan Cao ----</small>
+            <br />
+          </div>
+        </footer>
       </div>
     )
   }
 }
-
-export default App
