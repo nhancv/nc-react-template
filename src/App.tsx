cc83c04857dd55415eb9f088e758b849c0c30b6d
Nhan Cao <nhancv92@gmail.com>
2018-10-09 18:13:00 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,22 @@
-import * as React from 'react'
-import './App.css'
-import Particles from 'react-particles-js'
-import { Button } from 'reactstrap'
+import * as React from 'react';
+import './App.css';
 
-export default class App extends React.Component {
-  render() {
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Particles className="App-particle" />
-
-          <div className="App-header-body">
-            <div className="App-logo" />
-            <div className="App-header-info">
-              <h1 className="App-title">Nhan Cao</h1>
-              <h2 className="App-subtitle">Mobile Developer</h2>
-              <h2 className="App-subtitle">nhancv92@gmail.com</h2>
-            </div>
-          </div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
         </header>
-        <div className="App-body">
-          <section>
-            <p>
-              To get started, edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <div>
-              <Button
-                color="primary"
-                onClick={() => {
-                  console.log('hello')
-                }}
-              >
-                Hello
-              </Button>
-            </div>
-          </section>
-        </div>
-
-        <footer className="App-footer">
-          <div>
-            <br />
-            <small>---- Nhan Cao ----</small>
-            <br />
-          </div>
-        </footer>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
       </div>
-    )
+    );
   }
 }
+
+export default App;
