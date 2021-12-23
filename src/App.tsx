c06f5e99f85f26efe218e14c168cc8ffce323fb5
Nhan Cao <nhancv92@gmail.com>
2021-12-23 11:30:57 +0700

    feat: update template, tslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import React from 'react'
+import logo from './logo.svg'
+import './App.scss'
 
 type AppProps = {}
 type AppState = {}
 
 class App extends React.Component<AppProps, AppState> {
-
   constructor(props: AppProps) {
-    super(props);
-    this.state = {}
+    super(props)
+    this.state = { }
   }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
+          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.tsx</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
             Learn React
           </a>
         </header>
