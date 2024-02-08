b5f7fd426a161ecc02a9624ca9d66559f22c5884
Nhan Cao <nhancv92@gmail.com>
2024-02-08 14:43:55 +0700

    feat: update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -7,11 +7,12 @@ import * as serviceWorker from './serviceWorker';
 import ReactGA from 'react-ga4';
 ReactGA.initialize(process.env.REACT_APP_GA4_MEASUREMENT_ID || '');
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
